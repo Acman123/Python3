d4967b47880598dd8855c32fe582bc37972c68fc
Acman123 <acman1231@gmail.com>
2017-12-23 04:57:32 +0000

    Create Prime Factorisation.py

---
+++ Functions/Prime Factorisation.py
@@ -0,0 +1,21 @@
+def primeFactors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+   
+def largestPrimeFactor(n):
+    i = 2
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+    return n
