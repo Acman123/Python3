4383916dbad7c17b40192141bdf4e80383582f25
Acman123 <acman1231@gmail.com>
2018-04-26 22:39:17 +0100

    Create Type Test Decorators.py

---
+++ Functions/Decorators/Type Test Decorators.py
@@ -0,0 +1,63 @@
+def intTest(f):
+    def helper(x):
+        if isinstance(x,int):
+            return f(x)
+        else:
+            raise ValueError("Argument is not an integer")
+    return helper
+
+def realTest(f):
+    def helper(x):
+        if isinstance(x,float):
+            return f(x)
+        else:
+            raise ValueError("Argument is not an integer")
+    return helper
+
+def strTest(f):
+    def helper(x):
+        if isinstance(x,str):
+            return f(x)
+        else:
+            raise ValueError("Argument is not an integer")
+    return helper
+
+def arrayTest(f):
+    def helper(x):
+        if isinstance(x,list):
+            return f(x)
+        else:
+            raise ValueError("Argument is not an integer")
+    return helper
+
+def posTestWith0(f):
+    def helper(x):
+        if x >= 0:
+            return f(x)
+        else:
+            raise ValueError("Argument is not positive")
+    return helper
+
+def posTest(f):
+    def helper(x):
+        if x > 0:
+            return f(x)
+        else:
+            raise ValueError("Argument is not positive")
+    return helper
+
+def negTestWith0(f):
+    def helper(x):
+        if x <= 0:
+            return f(x)
+        else:
+            raise ValueError("Argument is not positive")
+    return helper
+
+def negTest(f):
+    def helper(x):
+        if x < 0:
+            return f(x)
+        else:
+            raise ValueError("Argument is not positive")
+    return helper
