e924775960f1699adcc9a359c74d58385e01e805
Acman123 <acman1231@gmail.com>
2018-04-26 19:26:00 +0100

    Create 2D Function Decorator.py

---
+++ Functions/Decorators/2D Function Decorator.py
@@ -0,0 +1,17 @@
+class Func2D():
+    def __init__(self, func):
+        self.f = func
+        self.xos = 0
+        self.yos = 0
+        
+    def __call__(self,x):
+        return self.f(x)
+
+    def offSet(self, xos=0, yos=0):
+        self.xos = xos
+        self.yos = yos
+
+        def __call__(x):
+            return self.f(x-self.xos) + self.yos
+
+        return __call__
