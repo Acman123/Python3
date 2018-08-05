5739c4dbd7e8f78d6b6700c64c64719382c5209d
Acman123 <acman1231@gmail.com>
2018-08-05 23:56:50 +0100

    Update Timer Decorator.py

--- Functions/Decorators/Timer Decorator.py
+++ Functions/Decorators/Timer Decorator.py
@@ -9,3 +9,19 @@ def timeit(method):
         print("{} took {:.2f}ms to run.".format(method.__name__,timed))
         return result
     return timed
+
+def avgTimeit(method,n):
+    def timed(*args, **kw):
+        values = []
+        for i in range(n):
+            ts = time.time()
+            result = method(*args, **kw)
+            te = time.time()
+            timed = (te - ts) * 1000
+            values.append(timed)
+
+        valuesRange = max(values) - min(values)
+        avg = sum(values)/n
+        print("{} took {:.2f}ms to run, with a range of {:.2f}ms.".format(method.__name__, avg, valuesRange))
+        return result
+    return timed
